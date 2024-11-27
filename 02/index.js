05fed5c30f637e181ab71952b837ecb0eaea74a9
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-27 22:20:40 +0600

    добавлен маршрут для шифрования пароля

--- 02/index.js
+++ 02/index.js
@@ -3,3 +3,6 @@ const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
 const PASSWORD = 'pass-number';
+app.get('/encode/:text', (req, res) => {
+    res.send(Vigenere.Cipher(PASSWORD).crypt(req.params.text));
+});
